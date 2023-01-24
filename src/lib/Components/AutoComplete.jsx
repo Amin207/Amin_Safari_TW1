11db26eb96b49922825f588d3487ed91d8de9ff0
fatemeh ezzati <fatemehezzati121@gmail.com>
2023-01-25 00:08:35 +0330

    ui v1

--- src/lib/Components/AutoComplete.jsx
+++ src/lib/Components/AutoComplete.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 import Select from 'react-select';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' },
-];
 
-
-export default function AutoComplete({countryData}) {
+export default function AutoComplete({ countryData }) {
   console.log(countryData)
 
   const [selectedOption, setSelectedOption] = useState(null);
+
+  const options = countryData.map(item => {
+    return { value: item.name.common, label: item.name.common }
+  })
+
+  //  const [flags , setFlags ] = useState()
+
 
   return <div className="autoComplete">
     <header>AutoComplete</header>
