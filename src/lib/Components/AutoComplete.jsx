051beb136fd9342ead200142dca8fe1e07bd17a7
AminS <safarin207@gmail.com>
2023-01-25 15:29:56 +0330

    add sort to country list

--- src/lib/Components/AutoComplete.jsx
+++ src/lib/Components/AutoComplete.jsx
@@ -6,22 +6,30 @@ import _ from "lodash";
 import api from "../Api/Api";
 
 import useStore from "../useStore";
-import shallow from "zustand";
+import { shallow } from "zustand/shallow";
 
 export default function AutoComplete() {
-  const countryData = useStore((s) => s.countryData);
-  const updateState = useStore((s) => s.updateState);
+  const { countryData, updateState } = useStore(
+    (s) => ({ countryData: s.countryData, updateState: s.updateState }),
+    shallow
+  );
 
-  const options = countryData.map((item) => {
-    return {
-      value: item.name.common,
-      label: item.name.common,
-      lat: item.latlng[0],
-      lon: item.latlng[1],
-      map: item.maps.googleMaps,
-      flag: item.flags,
-    };
-  });
+  const options = countryData
+    .sort((a, b) => {
+      const x = a.name.common.replace(/\s/g, "").toLowerCase();
+      const y = b.name.common.replace(/\s/g, "").toLowerCase();
+      return x > y ? 1 : x < y ? -1 : 0;
+    })
+    .map((item) => {
+      return {
+        value: item.name.common,
+        label: item.name.common,
+        lat: item.latlng[0],
+        lon: item.latlng[1],
+        map: item.maps.googleMaps,
+        flag: item.flags,
+      };
+    });
 
   const handleOnchange = (e) => {
     updateState("selectedCountry", e);
